c97c3e9e9c5ca9d5832cad68844cfd5879f537de
Dan Braghis <dan@zerolab.org>
2012-01-07 20:55:32 +0000

    Improve timemap initialization

--- js/timemap_init.js
+++ js/timemap_init.js
@@ -17,92 +17,103 @@ for (cc in lookup) {
 }
 lookup = null;
 
-function onLoad() {
+$(function(){
+
+  var height = $(window).height();
+  $('#mapcontainer').css('height', (height - 50) * .6 + 'px');
+  $('#timelinecontainer').css('height', (height - 50) * .4 + 'px');
+
+  $(window).resize(function(){
+    var height = $(window).height();
+    $('#mapcontainer').css('height', (height - 50) * .6 + 'px');
+    $('#timelinecontainer').css('height', (height - 50) * .4 + 'px');
+  });
+
   tm = TimeMap.init({
     mapId: "map",
     timelineId: "timeline",
     scrollTo: 'now',
     options: {
-		mapType: 'normal',
-		mapCenter: new GLatLng(30,18),
-		centerMapOnItems: false,
-		mapZoom: 2
-  },
-  datasets: [{
-    id: "bloggers",
-    title: "Bloggers",
-		type: "jsonp",
-    options: {
-      url: "http://threatened.globalvoicesonline.org/data/items.json&callback=",
-      preloadFunction: function(result) {return result.items;}
-    }
-  }],
-  bandIntervals: [
-    Timeline.DateTime.MONTH,
-    Timeline.DateTime.YEAR
-  ],
+		  mapType: 'normal',
+		  mapCenter: new GLatLng(30,18),
+		  centerMapOnItems: false,
+		  mapZoom: 2
+    },
+    datasets: [{
+      id: "bloggers",
+      title: "Bloggers",
+  		type: "jsonp",
+      options: {
+        url: "http://threatened.globalvoicesonline.org/data/items.json&callback=",
+        preloadFunction: function(result) {return result.items;}
+      }
+    }],
+    bandIntervals: [
+      Timeline.DateTime.MONTH,
+      Timeline.DateTime.YEAR
+    ],
 
-  dataDisplayedFunction: function(tm) {
-    tm.map.setZoom(2);
+    dataDisplayedFunction: function(tm) {
+      tm.map.setZoom(2);
 
-    tm.addFilterChain("counter",
-      function(item) {
-        var cc = item.opts.cc;
-        if (isNaN(countries[cc])) {
-          countries[cc] = 1;
-        }
-        else {
-          countries[cc]++;
-        }
+      tm.addFilterChain("counter",
+        function(item) {
+          var cc = item.opts.cc;
+          if (isNaN(countries[cc])) {
+            countries[cc] = 1;
+          }
+          else {
+            countries[cc]++;
+          }
 
-        // just in case the country code is not in cc.js
-  			if (typeof(cm[cc]) != "undefined") {
-  		    var index = cm[cc].items.indexOf(item.getTitle());
+          // just in case the country code is not in cc.js
+    			if (typeof(cm[cc]) != "undefined") {
+    		    var index = cm[cc].items.indexOf(item.getTitle());
 
-          if (!cm[cc].name)
-  					cm[cc].name = item.opts.c;
+            if (!cm[cc].name)
+    					cm[cc].name = item.opts.c;
 
-          if (index == -1) {
-            cm[cc].items.push(item.getTitle());
-  		  		cm[cc].urls.push(item.opts.url);}
-  				}
-      },
-      function(item) {}
-    );
+            if (index == -1) {
+              cm[cc].items.push(item.getTitle());
+    		  		cm[cc].urls.push(item.opts.url);}
+    				}
+        },
+        function(item) {}
+      );
 
-    tm.addFilter("counter", function(item) {
-      return TimeMap.filters.hideFuture(item);
-    });
+      tm.addFilter("counter", function(item) {
+        return TimeMap.filters.hideFuture(item);
+      });
 
-    // invoke the filter once data is displayed
-    tm.filter("counter");
+      // invoke the filter once data is displayed
+      tm.filter("counter");
 
-    if (!filters.length) {//show 'All'
-        for (cc in countries) {
-            if (countries[cc] > 0) {
-              dynamicSizeMarker(cc, countries[cc]);
-            }
-        }
-    }
+      if (!filters.length) {//show 'All'
+          for (cc in countries) {
+              if (countries[cc] > 0) {
+                dynamicSizeMarker(cc, countries[cc]);
+              }
+          }
+      }
 
-    updateTimeframe(tm.timeline.getBand(0));
+      updateTimeframe(tm.timeline.getBand(0));
 
-    $('.timeline-message-container').hide();
+      $('.timeline-message-container').hide();
 
       // update map on timeline scroll
       tm.timeline.getBand(0).addOnScrollListener(function() {
-    	  updateTimeframe(tm.timeline.getBand(0));
+      	updateTimeframe(tm.timeline.getBand(0));
         countries = {};
         tm.filter("counter");
         if (!filters.length) {//show 'All'
-        	for (cc in cm) {
-        	  if (countries[cc] > 0) {
-      			  dynamicSizeMarker(cc, countries[cc]);
-      			}
-      		  else if (cm[cc].marker) {
-      			  tm.map.removeOverlay(cm[cc].marker);
-        		  cm[cc].marker = null;
-          	}
+          for (cc in cm) {
+          	if (countries[cc] > 0) {
+        			dynamicSizeMarker(cc, countries[cc]);
+        		}
+        		else if (cm[cc].marker) {
+        			tm.map.removeOverlay(cm[cc].marker);
+          		cm[cc].marker = null;
+            }
           }
         }
       });
@@ -112,7 +123,77 @@ function onLoad() {
   tm.addFilter("map", TimeMap.filters.hasSelectedTag); // hide map markers on fail
   tm.addFilter("timeline", TimeMap.filters.hasSelectedTag); // hide timeline events on fail
   tm.addFilter("map", TimeMap.filters.showAll);
-}
+
+  $('#tm-all').attr('checked', true);
+  $("#tmfilters input[type=checkbox]:not('#tm-all')").each(function() {
+    $(this).attr('checked', false);
+  });
+  // TODO rework the logic.
+  $("#tmfilters input[type=checkbox]").click(function(){
+    var val = $(this).val();
+    var index = filters.indexOf(val);
+    var el = $(this);
+    if (el.is(':checked')) {
+      if (val == "All") {
+        filters = [];
+
+        $("#tmfilters input[type=checkbox]:not('#tm-all')").each(function() {
+            $(this).attr('checked', false);
+        });
+      }
+      else {
+        var _all = $('#tm-all');
+        if (_all.is(':checked')) {
+          _all.attr('checked', false);
+        }
+        removeSizeMarkers();
+
+        if (index == -1) {
+          filters.push(val);
+
+          tm.filter('timeline');
+          tm.timeline.layout();
+
+          tm.datasets.bloggers.each( function(item) {
+            for (_filter in filters) {
+              if (item.opts.tags.indexOf(filters[_filter]) != -1) {
+                item.showPlacemark();
+                item.visible = true;
+              }
+            }
+          });
+
+        return;
+        }
+      }
+    } // element is not checked
+    else {
+      if (index != -1) {
+        filters.splice(index, 1);
+      }
+    }
+
+    tm.filter('map');tm.filter('timeline');tm.timeline.layout();
+  });
+
+  var d = new Date();
+  $('#tm-nav button').click(function() {
+    var when = $(this).attr('title');
+
+    if (when == "earliest") {
+      tm.timeline.getBand(0).setCenterVisibleDate(tm.eventSource.getEarliestDate());
+    }
+    else if (when == "latest") {
+      tm.timeline.getBand(0).setCenterVisibleDate(tm.eventSource.getLatestDate());
+    }
+    else { //scroll to now
+      tm.timeline.getBand(0).setCenterVisibleDate(d);
+    }
+
+    return false;
+  });
+
+});
 
 TimeMap.filters.hideFuture = function(item) {
     var topband = item.dataset.timemap.timeline.getBand(0);
@@ -202,88 +283,6 @@ function removeSizeMarkers(){
 		}
 	}
 }
-
-$(document).ready(function(){
-
-  var height = $(window).height();
-  $('#mapcontainer').css('height', (height - 50) * .6 + 'px');
-  $('#timelinecontainer').css('height', (height - 50) * .4 + 'px');
-
-  $(window).resize(function(){
-    var height = $(window).height();
-    $('#mapcontainer').css('height', (height - 50) * .6 + 'px');
-    $('#timelinecontainer').css('height', (height - 50) * .4 + 'px');
-  });
-
-	$('#tm-all').attr('checked', true);
-	$("#tmfilters input[type=checkbox]:not('#tm-all')").each(function() {
-		$(this).attr('checked', false);
-	});
-	// TODO rework the logic.
-	$("#tmfilters input[type=checkbox]").click(function(){
-		var val = $(this).val();
-		var index = filters.indexOf(val);
-		var el = $(this);
-		if (el.is(':checked')) {
-			if (val == "All") {
-				filters = [];
-
-				$("#tmfilters input[type=checkbox]:not('#tm-all')").each(function() {
-						$(this).attr('checked', false);
-				});
-			}
-			else {
-				var _all = $('#tm-all');
-				if (_all.is(':checked')) {
-          _all.attr('checked', false);
-        }
-				removeSizeMarkers();
-
-        if (index == -1) {
-          filters.push(val);
-
-		      tm.filter('timeline');
-          tm.timeline.layout();
-
-        	tm.datasets.bloggers.each( function(item) {
-						for (_filter in filters) {
-	            if (item.opts.tags.indexOf(filters[_filter]) != -1) {
-	              item.showPlacemark();
-	              item.visible = true;
-						  }
-						}
-        	});
-
-        return;
-        }
-			}
-		} // element is not checked
-		else {
-			if (index != -1) {
-				filters.splice(index, 1);
-			}
-		}
-
-		tm.filter('map');tm.filter('timeline');tm.timeline.layout();
-	});
-
-	var d = new Date();
-	$('#tm-nav button').click(function() {
-		var when = $(this).attr('title');
-
-		if (when == "earliest") {
-			tm.timeline.getBand(0).setCenterVisibleDate(tm.eventSource.getEarliestDate());
-		}
-		else if (when == "latest") {
-			tm.timeline.getBand(0).setCenterVisibleDate(tm.eventSource.getLatestDate());
-		}
-		else { //scroll to now
-			tm.timeline.getBand(0).setCenterVisibleDate(d);
-		}
-
-		return false;
-	});
-});
 
 function updateTimeframe(bandObj) {
 	var min = new String(bandObj.getMinVisibleDate()).split(/\s/);
